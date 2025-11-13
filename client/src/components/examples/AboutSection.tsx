83febd5922e7a62c3b6eb874c69a3637d3ebb4e7
Ravi Mahajan <ravi.d.mahajan@gmail.com>
2025-11-13 07:14:53 -0600

    fix

--- client/src/components/examples/AboutSection.tsx
+++
@@ -1,9 +0,0 @@
-import AboutSection from "../AboutSection";
-
-export default function AboutSectionExample() {
-  return (
-    <AboutSection
-      about="I'm a passionate professional dedicated to creating meaningful digital experiences. With years of experience in design and development, I combine creativity with technical expertise to bring ideas to life. When I'm not coding, you'll find me exploring new technologies, contributing to open-source projects, and continuously learning to stay at the forefront of innovation."
-    />
-  );
-}
